769a13f81256da2876970365411fb4ec997cab63
manohar-git-hub <kundamanohar@gmail.com>
2023-10-09 11:22:05 +0530

    First Commit: Springboot to MySQL

---
+++ Documents/STS/SB-MYSQL/SpringBootMySQL/src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+
+	public List<User> saveUsers(List<User> users) {
+		return userRepository.saveAll(users);
+	}
+
+	public List<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	public User getUserById(int id) {
+		return userRepository.findById(id).orElse(null);
+	}
+
+	public User getUserByName(String name) {
+		return userRepository.findByName(name);
+	}
+
+	public String deleteUser(int id) {
+		userRepository.deleteById(id);
+		return "User removed !! " + id;
+	}
+}
